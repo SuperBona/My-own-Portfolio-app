11592c3a261b31274255f293f9f1f7398bd80702
Claudia Esposito <silvia@troia.it>
2023-09-07 16:20:28 +0200

    fixing error 404

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Navbar></Navbar>
       <Themes></Themes>
       <Routes>
-        <Route index element={<Home />}></Route>
+        <Route index exact path='/' element={<Home />}></Route>
         <Route path='about' element={<About />}></Route>
         <Route path='portfolio' element={<Portfolio />}></Route>
         <Route path='contact' element={<Contact />}></Route>
